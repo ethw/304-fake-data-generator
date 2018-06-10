ca5baa8d4939672deae8de00ae02d6a1d3689bd0
root <root@E-Desktop.localdomain>
2018-06-10 02:27:07 -0700

    Lots of refactoring and organization and more fake data

--- generator.js
+++ generator.js
@@ -1,14 +1,104 @@
-var id = 1;
-var adjectives = ['Attractive', 'Beautiful', 'Chubby', 'Dazzling', 'Magnificent', 'Plump',
-'Ashy', 'Black', 'Icy', 'Red', 'Best', 'Delicious', 'Jolly', 'Wonderful', 'Mysterious']
-var the = 'The'
-var typesOfRestaurants = ['Pizzeria', 'Pancake House', 'Taco City', 'Sandwichery', 'Creamery']
-var names = ['Mary', 'Patricia', 'Linda', 'Barbara', 'Jennifer', 'Maria', 'Susan', 'James', 'John', 'Robert', 'Michael', 'William', 'David']
-var foodTypes = ['Burger', 'Curry', 'Fries', 'Taco', 'Salad', 'Burrito', 'Sandwich']
-var streetNames = ['Granville', 'Arbutus', 'Davie', 'Broadway', 'Cambie', 'Marine']
-var streetTypes = ['Dr', 'St', 'Cr']
+// Application entry point
+// Call this function to create fake data
+function createData(numStores, numCustomers) {
+  var sqlBuffer  = ''
 
-function getUniqueID() {
+  var cids = []
+  var sids = []
+  var fids = new Map()
+
+  // Generate Stores, StoreManagers, and randomly between 0-10 items per store
+  rangeOfSize(numStores).forEach(() => {
+    var sid = newID()
+    sids.push(sid)
+    fids.set(sid, [])
+
+    // Generating FoodItems
+    var foodBuffer = ''
+    var numItemsPerStore = randomFrom1To(10)
+    rangeOfSize(numItemsPerStore).forEach(() => {
+      var foodType = randomFrom(foodTypes)
+      var title = '"' + randomFrom(adjectives) + ' ' + foodType + '"'
+      var description = '"' + 'The tastiest ' + foodType + ' possible' + '"'
+      var fid = newID()
+      fids.get(sid).push(fid)
+      foodBuffer += wrapAsInsert('FoodItemOffers1', title, description)
+      foodBuffer += wrapAsInsert('FoodItemOffers2', fid, sid, title)
+    })
+
+    // Generate Manager
+    var smid = newID()
+    var firstName = randomFrom(names)
+    var lastName = randomFrom(names)
+    var smName = '"' + firstName + ' ' + lastName + '"'
+    var smPassword = '"' + lastName + smid + '"'
+    var smUsername = '"' + firstName + smid + '"'
+    var smAddress = '"' + randomStreet() + '"'
+
+    sqlBuffer  += wrapAsInsert('StoreManager', smid, sid, smUsername, smPassword, smName, smAddress)
+
+    // Generating Store and finishing up
+    var sName = '"' + 'The ' + randomFrom(adjectives) + ' ' + randomFrom(typesOfRestaurants) + '"'
+    var popularItem = fids.get(sid)[fids.get(sid).length - 1]
+    var sAddress = '"' + randomStreet() + '"'
+
+    sqlBuffer  += wrapAsInsert('Store1', sName, popularItem)
+    sqlBuffer  += wrapAsInsert('Store2', sid, smid, sName, sAddress)
+
+    sqlBuffer  += foodBuffer
+  })
+
+  // Generate customers
+  rangeOfSize(numCustomers).forEach(() => {
+    var cid = newID()
+    cids.push(cid)
+    var firstName = randomFrom(names)
+    var lastName = randomFrom(names)
+    var username = '"' + firstName + cid + '"'
+    var password = '"' + lastName + cid + '"'
+    var name = '"' + firstName + ' ' + lastName + '"'
+    var address = '"' + randomStreet() + '"'
+
+    sqlBuffer  += wrapAsInsert('Customer', cid, username, password, name, address)
+  })
+
+  // Generate orders and messages
+  sids.forEach(sid => {
+    var numOrders = randomFrom1To(10)
+    rangeOfSize(numOrders).forEach(() => {
+      var oid = newID()
+      var cid = randomFrom(cids)
+      var time = randomTime()
+      sqlBuffer  += wrapAsInsert('OrderFullfillsAndPlaces', oid, sid, cid, time)
+
+      var numItems = randomFrom1To(5)
+      rangeOfSize(numItems).forEach(() => {
+        sqlBuffer  += wrapAsInsert('Contains', oid, randomFrom(fids.get(sid)))
+      })
+
+      var mid = newID()
+      var subject = '"' + 'Your order #' + oid.toString() + '"'
+      var content = '"' + 'Your order is on its way. Enjoy!' + '"'
+      sqlBuffer  += wrapAsInsert('MessageSendsAndReceives', mid, sid, cid, subject, content, time)
+    })
+  })
+
+  try {
+    download('fakeData-' + numStores + 'stores-'+ numCustomers + 'customers.sql', sqlBuffer)
+  } catch {}
+
+  return sqlBuffer
+}
+
+var start = 'INSERT INTO '
+var middle = ' VALUES ('
+var end = ');\n'
+function wrapAsInsert(tableName, ...values) {
+  return start + tableName + middle + values.join(',') + end
+}
+
+var id = 1
+function newID() {
   return id++
 }
 
@@ -26,98 +116,28 @@ function randomFrom1To(n) {
 
 function randomTime() {
   var baseTime = (new Date).getTime()
-  var modifier = randomFrom1To(1000)
+  var modifier = randomFrom1To(1000000)
   return randomFrom1To(10) < 5 ? baseTime + modifier : baseTime - modifier
 }
 
-function createData(numStores, numCustomers) {
-  var buffer = ''
-  var numOrders = numCustomers * 5
-
-  var start = 'INSERT INTO '
-  var middle = ' VALUES ('
-  var end = ');\n'
-
-  var smids = []
-  var cids = []
-  var sids = []
-  var mids = []
-  var fids = new Map();
-  var oids = []
-
-  // Generate Stores, StoreManagers, and randomly between 0-10 items per store
-  Array.from(Array(numStores).keys()).forEach(i => {
-    var sid = getUniqueID()
-    sids.push(sid)
-    fids.set(sid, [])
-
-    var foodBuffer = '';
-    var numItemsPerStore = randomFrom1To(10)
-    Array.from(Array(numItemsPerStore).keys()).forEach(j => {
-      var foodType = randomFrom(foodTypes)
-      var title = '"' + randomFrom(adjectives) + ' ' + foodType + '"'
-      var description = '"' + 'The tastiest ' + foodType + ' possible' + '"'
-      var fid = getUniqueID()
-      fids.get(sid).push(fid)
-      foodBuffer += start + 'FoodItemOffers1' + middle + title + ',' + description + end
-      foodBuffer += start + 'FoodItemOffers2' + middle + fid + ',' + sid + ',' + title + end
-    })
-
-    var smid = getUniqueID()
-    smids.push(smid)
-    var firstName = randomFrom(names)
-    var smName = '"' + firstName + ' ' + randomFrom(names) + '"'
-    var smPasswd = '"' + smid + '"'
-    var smUsername = '"' + firstName + smid + '"'
-    var smAddress = '"' + randomStreet() + '"'
-
-    buffer += start + 'StoreManager' + middle + smid + ',' + sid + ',' + smUsername + ',' + smPasswd + ',' + smName + ',' + smAddress + end
-
-    var sName = '"' + the + ' ' + randomFrom(adjectives) + ' ' + randomFrom(typesOfRestaurants) + '"'
-    var popularItem = fids[fids.length - 1]
-    var sAddress = '"' + randomStreet() + '"'
-
-    buffer += start + 'Store1' + middle + sName + ',' + popularItem + end
-    buffer += start + 'Store2' + middle + sid + ',' + smid + ',' + sName + ',' + sAddress + end
-
-    buffer += foodBuffer
-  })
-
-  // Generate customers
-  Array.from(Array(numCustomers).keys()).forEach(index => {
-    var cid = getUniqueID()
-    cids.push(cid)
-    var firstName = randomFrom(names)
-    var username = '"' + firstName + cid + '"'
-    var password = '"' + cid + '"'
-    var name = '"' + firstName + ' ' + randomFrom(names) + '"'
-    var address = '"' + randomStreet() + '"'
-
-    buffer += start + 'Customer' + middle + cid + ',' + username + ',' + password + ',' + name + ',' + address + end
-  })
-
-  // Generate orders and messages
-  sids.forEach(sid => {
-    var numOrders = randomFrom1To(10)
-    Array.from(Array(numOrders).keys()).forEach(i => {
-      var oid = getUniqueID()
-      oids.push(oid)
-      var cid = randomFrom(cids)
-      var time = randomTime()
-      buffer += start + 'OrderFullfillsAndPlaces' + middle + oid + ',' + sid + ',' + cid + ',' + time + end
-
-      var numItems = randomFrom1To(5)
-      Array.from(Array(numItems).keys()).forEach(j => {
-        buffer += start + 'Contains' + middle + oid + ',' + randomFrom(fids.get(sid)) + end
-      })
-
-      var mid = getUniqueID()
-      mids.push(mid)
-      var subject = '"' + 'Your order ' + oid.toString() + '"'
-      var content = '"' + 'Your order is on its way. Enjoy!' + '"'
-      buffer += start + 'MessageSendsAndReceives' + middle + mid + ',' + sid + ',' + cid + ',' + subject + ',' + content + ',' + time + end
-    })
-  })
-
-  return buffer
+function rangeOfSize(n) {
+  return Array.from(Array(n).keys())
 }
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+// Data for random attribute generation
+var adjectives = ['Attractive', 'Beautiful', 'Chubby', 'Dazzling', 'Magnificent', 'Plump', 'Black', 'Icy', 'Red', 'Best', 'Delicious', 'Jolly', 'Wonderful', 'Mysterious', 'Succulent', 'Spicy', 'Natural', 'Fresh', 'Traditional', 'Healthy', 'Soft', 'Hot', 'Cold', 'Enjoyable', 'Amazing', 'Magical', 'Toasted']
+var typesOfRestaurants = ['Pizzeria', 'Pancake House', 'Taco City', 'Sandwichery', 'Creamery', 'Boulangerie', 'Patisserie', 'Brasserie', 'Barbecue', 'Tavern', 'Buffet', 'Cafe', 'Pub', 'Dive Bar']
+var names = ['Mary', 'Patricia', 'Linda', 'Barbara', 'Jennifer', 'Maria', 'Susan', 'James', 'John', 'Robert', 'Michael', 'William', 'David', 'Kelly', 'Cindy', 'Gary', 'Robyn', 'Hannah', 'Bill', 'Joey', 'Han', 'Harrison']
+var foodTypes = ['Burger', 'Curry', 'Fries', 'Taco', 'Salad', 'Burrito', 'Sandwich', 'Sushi', 'Ice Cream', 'Pulled Pork Sandwich', 'Coffee', 'Meatballs', 'Pasta', 'Calzone', 'Cake', 'Bagel', 'Milkshake', 'Chicken Strips']
+var streetNames = ['Granville', 'Arbutus', 'Davie', 'Broadway', 'Cambie', 'Marine', 'Yew', 'Burrard', 'Main', 'Thurlow', 'Sasamat', 'Blanca', 'Alma', 'Trafalgar', 'Discovery', 'Pender', 'Mainland', 'Hamilton', 'Pacific', 'Beach', 'Eastdown', 'Gibbins', 'Highland', 'Carmel', 'Upland']
+var streetTypes = ['Dr', 'St', 'Cr', 'Way', 'Lane', 'Grove', 'Place', 'Terrace', 'Hill', 'Square', 'Junction', 'Heights', 'Gardens', 'Creek', 'Center', 'Canyon', 'Avenue', 'Boulevard', 'Alley']
